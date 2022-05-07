780abf4c7282a603e52dabbc3904e00a03c733c8
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2022-05-07 01:23:52 -0500

    Added 3 more definitions, and doing a quick setup

--- functions.py
+++ functions.py
@@ -14,8 +14,31 @@ def backup():
             response = str(input('\t Please input your selection: '))
 
             if response == '1':
-                subprocess.run(['mysqldump --user=root --password=Admin1234! --lock-tables --all-databases > server_db_backup.sql'], shell=True)
+                subprocess.run(['mysqldump --user=USERNAME --password=PASSWORD --lock-tables --all-databases > server_db_backup.sql'], shell=True)
                 subprocess.run(['tar -zcvf "$(date '+%Y-%m-%d').tar.gz" server_db_backup.sql'], shell=True)
                 subprocess.run(['rm server_db_backup.sql'], shell=True)
             elif response == '2':
                 break
+
+def transferBackup():
+
+
+def restoreBackup():
+
+
+
+def return_to_loop():
+	while True:
+					
+		print('\n\n ----------------------------------------------------------------------------\n')
+		print('\t\t\t\t Orbb')
+		print('\n\n\t Would you like to return to the main menu?\n\t')
+		print('\t 1. Yes')
+		print('\t 2. No')
+		
+		response = str(input('\n\t Response: '))
+		if response == '1':
+			break
+		elif response == '2':
+			subprocess.run(['clear'], shell=True)
+			sys.exit() 				# This causes the program to terminate gracefully.
