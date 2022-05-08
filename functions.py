b953cd35115c31fb4ded567944557e9b95c2d1ad
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2022-05-08 16:49:47 -0500

    Code uniformity, removed a few tabs

--- functions.py
+++ functions.py
@@ -4,59 +4,58 @@ import sys, subprocess
 def backup():
 	while True:
 
-			# Create a temporary directory that will be used throughout the script.
+		# Create a temporary directory that will be used throughout the script.
 
-			subprocess.run(['mkdir /tmp/Backup/ && mkdir /tmp/Backup/etc/ && mkdir /tmp/Backup/usr'], shell=True)
+		subprocess.run(['mkdir /tmp/Backup/ && mkdir /tmp/Backup/etc/ && mkdir /tmp/Backup/usr'], shell=True)
 
-			print('\n\t NGINX settings, the mariaDB database, letsencrypt SSL certs, \n\t and the postfix configs will be backed up.\n')
-			print('\t Is this something you wanted to do?\n')
+		print('\n\t NGINX settings, the mariaDB database, letsencrypt SSL certs, \n\t and the postfix configs will be backed up.\n')			print('\t Is this something you wanted to do?\n')
+		print('\t 1: Yes')
+		print('\t 2: No\n')
+
+		response = str(input('\t Please input your selection: '))
+
+		if response == '1':
+
+			subprocess.run(['clear'], shell=True)
+			print('\t Backup initiated, please wait, as this may take some time depending on your CPU speed...\n\t')
+
+			# Start the backup process of the mariaDB database.
+
+			subprocess.run(['cd /tmp/Backup/ && mysqldump --user=root --password=Admin1234! --lock-tables --all-databases > server_db_backup.sql'], shell=True)
+
+			# Starts the backup process of my.cnf, NGINX, and postfix for the mail system / SendGrid settings, then moves them in the tmp directory.
+
+			subprocess.run(['cp /etc/my.cnf /tmp/Backup/etc/'], shell=True)
+			subprocess.run(['sudo cp -r /etc/nginx/ /tmp/Backup/etc/'], shell=True)
+			subprocess.run(['sudo cp -r /etc/postfix/ /tmp/Backup/etc/'], shell=True)
+
+			# Starts the backup process of the website, and its included files. This may take long depending on what's in there.
+
+			subprocess.run(['sudo cp -r /usr/share/nginx/ /tmp/Backup/usr/'], shell=True)
+
+			# Starts the backup process of the letsencrypt certs for the website's SSL
+
+			subprocess.run(['sudo cp -r /etc/letsencrypt/ /tmp/Backup/etc/'], shell=True)
+
+			# Compresses the /tmp/Backup/ folder for RSYNC later on.
+
+			subprocess.run(['cd /tmp/ && sudo tar -zcvf "ServerBackup.tar.gz" /tmp/Backup '], shell=True)
+			subprocess.run(['cd /tmp/ && sudo rm -rf Backup/'], shell=True)
+
+			print('\n\t Backup has been completed, would you like to return to the main menu?\n')
 			print('\t 1: Yes')
 			print('\t 2: No\n')
 
 			response = str(input('\t Please input your selection: '))
 
 			if response == '1':
-
-				subprocess.run(['clear'], shell=True)
-				print('\t Backup initiated, please wait, as this may take some time depending on your CPU speed...\n\t')
-
-				# Start the backup process of the mariaDB database.
-
-				subprocess.run(['cd /tmp/Backup/ && mysqldump --user=root --password=Admin1234! --lock-tables --all-databases > server_db_backup.sql'], shell=True)
-
-				# Starts the backup process of my.cnf, NGINX, and postfix for the mail system / SendGrid settings, then moves them in the tmp directory.
-
-				subprocess.run(['cp /etc/my.cnf /tmp/Backup/etc/'], shell=True)
-				subprocess.run(['sudo cp -r /etc/nginx/ /tmp/Backup/etc/'], shell=True)
-				subprocess.run(['sudo cp -r /etc/postfix/ /tmp/Backup/etc/'], shell=True)
-
-				# Starts the backup process of the website, and its included files. This may take long depending on what's in there.
-
-				subprocess.run(['sudo cp -r /usr/share/nginx/ /tmp/Backup/usr/'], shell=True)
-
-				# Starts the backup process of the letsencrypt certs for the website's SSL
-
-				subprocess.run(['sudo cp -r /etc/letsencrypt/ /tmp/Backup/etc/'], shell=True)
-
-				# Compresses the /tmp/Backup/ folder for RSYNC later on.
-
-				subprocess.run(['cd /tmp/ && sudo tar -zcvf "ServerBackup.tar.gz" /tmp/Backup '], shell=True)
-				subprocess.run(['cd /tmp/ && sudo rm -rf Backup/'], shell=True)
-
-				print('\n\t Backup has been completed, would you like to return to the main menu?\n')
-				print('\t 1: Yes')
-				print('\t 2: No\n')
-
-				response = str(input('\t Please input your selection: '))
-
-				if response == '1':
-					break
-				elif response == '2':
-					subprocess.run(['clear'], shell=True)
-					sys.exit()
-
-			elif response == '2':
 				break
+			elif response == '2':
+				subprocess.run(['clear'], shell=True)
+				sys.exit()
+
+		elif response == '2':
+			break
 
 
 
