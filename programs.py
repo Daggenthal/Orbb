ef293f667e6e2f1d3b82b6e65a5183e442ec8af5
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2022-06-12 03:26:38 -0500

    Removed `ubuntu` as I found out how to call it within a list.
    Also removed `upv` as I can now call it in the list

--- programs.py
+++ programs.py
@@ -8,21 +8,17 @@ distro = ['debian', 'ubuntu', 'fedora', 'arch', 'opensuse', 'freebsd']
 
 debian = 'sudo apt install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
-ubuntu = 'sudo apt install nginx mariadb-server memcached certbot postfix pv php-cli python3-certbox-nginx httpd'
-
 fedora = 'sudo dnf install -y epel-release && sudo dnf install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
 arch = 'sudo pacman -S --noconfirm nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
 opensuse = 'sudo zypper install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
-freebsd = 'sudo pkg install nginx mariadb106-server-10.6.8 memcached postfix py38-certbot-nginx-1.22.0 apache24-2.4.54'
+freebsd = 'sudo pkg install nginx mariadb106-server-10.6.8 mariadb106-client-10.6.8  memcached postfix py38-certbot-nginx-1.22.0 apache24-2.4.54'
 
 #############################################################################################################################################################
 
 dpv = 'sudo apt install -y pv'
-
-upv = 'sudo apt install -y pv'
 
 fpv = 'sudo dnf install -y pv'
 
