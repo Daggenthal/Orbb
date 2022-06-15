11e23a71b6e8c19d8c124856ea9e58c818a6aa07
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2022-06-15 11:01:41 -0500

    Removed auto-confirm for software installation.

--- programs.py
+++ programs.py
@@ -6,15 +6,15 @@ OS = getoutput(["cat /etc/os-release | awk 'NR==3'"])
 
 distro = ['debian', 'ubuntu', 'fedora', 'rocky', 'arch', 'opensuse', 'freebsd']
 
-debian = 'sudo apt install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
+debian = 'sudo apt install nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
-fedora = 'sudo dnf install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
+fedora = 'sudo dnf install nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
-rocky = 'sudo dnf install -y epel-release && sudo dnf install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
+rocky = 'sudo dnf install -y epel-release && sudo dnf install nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
-arch = 'sudo pacman -S --noconfirm nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
+arch = 'sudo pacman -S nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
-opensuse = 'sudo zypper install -y nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
+opensuse = 'sudo zypper install nginx mariadb-server memcached certbot postfix pv php-cli python3-certbot-nginx httpd'
 
 freebsd = 'sudo pkg install nginx mariadb106-server-10.6.8 mariadb106-client-10.6.8  memcached postfix py38-certbot-nginx-1.22.0 apache24-2.4.54'
 
